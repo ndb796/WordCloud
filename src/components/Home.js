7e2ebbe09ca8a47074a870d3dae1b1f466690986
ndb796 <ndb796@naver.com>
2019-04-24 09:28:31 +0900

    Add Router Module

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <Card>
+                <CardContent>
+                    React 및 Firebase 기반의 워드 클라우드 어플리케이션
+                </CardContent>
+            </Card>
+        );
+    }
+}
+
+export default Home;
